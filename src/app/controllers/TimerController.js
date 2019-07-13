407660f2864958efa737e8a0d8829b6f2231a6e0
Gabriel Antiqueira <gmantiqueira@outlook.com>
2019-07-13 02:08:13 -0300

    feature WIP | Controle do Timer

--- src/app/controllers/TimerController.js
+++ src/app/controllers/TimerController.js
@@ -1,62 +1,89 @@
 const Session = require("../models/Session");
+var moment = require('moment')
 
 class TimerController {
   async pauseTimer(req, res) {
-    await Session.findByIdAndUpdate(req.params.id, {
-      isPaused: true
+    const session = await Session.findByIdAndUpdate(req.params.id, {
+      isPaused: true,
+      delayStart: Date.now()
     });
 
     const pauseTimer = await Session.findById(req.params.id);
 
     req.io.emit("pauseTimer", pauseTimer);
 
-    return res.send();
+    return res.json(session);
   }
 
   async resumeTimer(req, res) {
-    await Session.findByIdAndUpdate(req.params.id, {
-      isPaused: false
+    var info = await Session.findById(req.params.id)
+
+    var newEndline = info.endline.setMilliseconds(info.endline.getMilliseconds() + (Date.now() - info.delayStart.getTime()))
+
+    const session = await Session.findByIdAndUpdate(req.params.id, {
+      isPaused: false,
+      endline: newEndline
     });
+
+    // console.log(info.endline.getMilliseconds())
 
     const resumeTimer = await Session.findById(req.params.id);
 
     req.io.emit("resumeTimer", resumeTimer)
 
-    return res.send();
+    return res.json(session);
   }
 
   async setTimer(req, res) {
     const { totalTime } = req.body;
-    var dateNow = new Date();
+
+    var endline = moment(moment().add(totalTime, 'm'))
 
     await Session.findByIdAndUpdate(req.params.id, {
-      endline: dateNow.setMinutes(dateNow.getMinutes() + totalTime),
+        totalTime: totalTime,
+        endline: endline,
+        isRunning: true,
+        isPaused: false
     });
 
     const setTimer = await Session.findById(req.params.id);
 
     req.io.emit("setTimer", setTimer);
 
-    return res.json();
+    return res.json(setTimer);
   }
 
   async updateTimer(req, res) {
-    const { totalTime } = req.body;
     var dateNow = new Date();
 
+    const info = await Session.findById(req.params.id);
+
+    var isRunning = dateNow.getTime() >= info.endline.getTime()
+    ? false
+    : true
+
     await Session.findByIdAndUpdate(req.params.id, {
-      totalTime: totalTime,
-      isRunning:
-        dateNow >= dateNow.setMinutes(dateNow.getMinutes() + totalTime)
-          ? false
-          : true
+      isRunning: isRunning
     });
 
-    const updateTimer = await Session.findById(req.params.id);
 
-    req.io.emit("updateTimer", updateTimer);
+    req.io.emit("updateTimer", info);
 
-    return res.json();
+    return res.json(info);
+  }
+
+  async stopTimer(req, res) {
+    await Session.findByIdAndUpdate(req.params.id, {
+        endline: Date.now(),
+        isRunning: false,
+        isPaused: false
+    });
+
+    const stop = await Session.findById(req.params.id)
+
+    req.io.emit("stopTimer", stop);
+
+    return res.json(stop);
   }
 }
 
