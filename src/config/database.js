b6e27531c56e3e8e55cb07e27452e8e084939ce7
Gabriel Antiqueira <gmantiqueira@outlook.com>
2019-07-24 00:00:50 -0300

    hotfix | Deploy Heroku

--- src/config/database.js
+++ src/config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    uri: 'mongodb://localhost:27017/timer_api'
+    uri: 'mongodb+srv://gmantiqueira:49ppF3dUEiQpEWGj@cluster0-9df7c.mongodb.net/test?retryWrites=true&w=majority'
+    // uri: 'mongodb://localhost:27017/timer_api'
   }
-  
